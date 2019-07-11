43a0b4de442a4bd8b6ed225a0f5327a3c3f40aa2
soundarya-1609 <51950558+soundarya-1609@users.noreply.github.com>
2019-07-11 12:06:01 +0530

    Create pro8.py

---
+++ pro8.py
@@ -0,0 +1,8 @@
+chats=int(input())
+chots=[int(o) for o in input().split(" ")]
+chets=0
+for p in range(chast):
+      for g in range(p):
+           if(chots[g]<chots[p]):
+                chets+=chots[g]
+print(chets)
